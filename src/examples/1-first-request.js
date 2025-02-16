46c20814591f4d9e49cd5f65d76af8671b17674c
emmeiwhite <emmeiwhite@gmail.com>
2025-02-16 18:23:44 +0530

    axios.1.1 a basic get request with axios

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
+import axios from 'axios'
 // limit, if 429 wait for 15 min and try again
-const url = 'https://www.course-api.com/react-store-products';
+const url = 'https://www.course-api.com/react-store-products'
 
 const FirstRequest = () => {
-  useEffect(() => {
-    console.log('first axios request');
-  }, []);
+  // fetching the API data with axios
 
-  return <h2 className='text-center'>first request</h2>;
-};
-export default FirstRequest;
+  async function fetchData() {
+    console.log('inside the fetchData function')
+    try {
+      const response = await axios(url)
+      console.log(response)
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+
+  // useEffect gets invoked after the initial render for the component
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return <h2 className="text-center">first request</h2>
+}
+export default FirstRequest
