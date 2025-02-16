dc26ac3295d0a7c6952d90e42519eda4956fd99e
emmeiwhite <emmeiwhite@gmail.com>
2025-02-17 00:15:26 +0530

    axios-1.5 successfully setup the custom instance with axios.

--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,16 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
+import axios from 'axios'
 
-const randomUserUrl = 'https://randomuser.me/api';
+import authFetch from '../axios/custom'
+const randomUserUrl = 'https://randomuser.me/api'
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
-  };
+    //  Lets make 2 API calls : one from the custom axios instance and one from normal axios call
+
+    try {
+      const response1 = await authFetch('/react-store-products')
+      const response2 = await axios.get(randomUserUrl)
+    } catch (error) {}
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
-  return <h2 className='text-center'>custom instance</h2>;
-};
-export default CustomInstance;
+  return <h2 className="text-center">custom instance</h2>
+}
+export default CustomInstance
