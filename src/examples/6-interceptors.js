cb4fba863d02b86860aa07dc553adc8d73777a88
emmeiwhite <emmeiwhite@gmail.com>
2025-02-18 13:20:33 +0530

    axios-1.7 interceptor error handling globally

--- src/examples/6-interceptors.js
+++ src/examples/6-interceptors.js
@@ -6,7 +6,7 @@ const url = 'https://www.course-api.com/react-store-products'
 const Interceptors = () => {
   const fetchData = async () => {
     try {
-      const resp = await authFetch('/react-store-products')
+      const resp = await authFetch('/react-store-productss')
     } catch (error) {}
   }
 
